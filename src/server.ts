19304484d2a3ffded173a336ed2cd8c8268cb689
Rob Arnhart <rob@arnhart.com>
2020-10-25 16:25:22 -0500

    Added health endpoint

--- src/server.ts
+++ src/server.ts
@@ -36,6 +36,9 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
     res.send( '/api/v0/' );
   } );
 
+  app.get("/health", (req, res, next) => {
+    res.status(200).send("Hello");
+  });
 
   // Start the Server
   app.listen( port, () => {
